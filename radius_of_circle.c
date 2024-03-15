6edce97e70060938f385dc620b6d31e2687dc7cc
ShraddhaSoudagar <130531532+ShraddhaSoudagar@users.noreply.github.com>
2024-03-15 16:10:23 +0530

    Add files via upload

---
+++ radius_of_circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float r,c;
+    printf("Enter radius of circle: ");
+    scanf("%f", &r);
+    c=2*3.14*r;
+    printf("circumference of circle = %.2f units \n", c);
+
+
+    return 0;
+}
